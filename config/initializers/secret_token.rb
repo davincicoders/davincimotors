844eedf46d3ed2fca8196342d63ea95b805b874c
Jason Noble <github+jasonn@jasonnoble.org>
2013-08-11 22:56:49 -0600

    Initial commit for DaVinci Motors

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DavinciMotors::Application.config.secret_key_base = '69edcccdd7f5b089e4f3dad0ae8d2ac1637054e47b43de5e2ff9e25a5e00361b5a550a8ca49111900d4bcc6f41553430e3373c4e7d780d7ded6b473e37c1b4c1'
